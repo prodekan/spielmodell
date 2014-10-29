0a3c950eb04bde699202afcfd94429e7ca808500
Selver B <selver.basic@gmail.com>
2014-10-29 22:40:59 +0100

    parse

--- htmlBLParser/tipico_webscrape.R
+++ htmlBLParser/tipico_webscrape.R
@@ -3,7 +3,8 @@ connectToSite <- function()
 {
   setwd('/home/selver/PycharmProjects/htmlBLParser/');
   htmlP <- htmlTreeParse(file = 'tipico_bl1.html', useInternalNodes = T);
-  title <- xpathApply(htmlP, "//title", xmlValue);
-
+  search <- '//div'
+  title <- xpathApply(htmlP,  search, xmlValue);
+  print(title)
 }
 
